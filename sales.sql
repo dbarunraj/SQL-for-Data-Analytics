3956b63268e533b9261fcded0de0d949e037c436
DB ARUN RAJ <70089211+dbarunraj@users.noreply.github.com>
2021-07-03 15:02:35 +0530

    Add files via upload

---
+++ sales.sql
@@ -0,0 +1,3 @@
+CREATE TABLE PHONE_SHOP (brand varchar(200), model_name varchar(200),selling_price int, customer_age int, quantity int);
+INSERT INTO phone_shop VALUES('samsung', 'redmi note 10','8999','22','9'),('Apple','iphone SE','24999','25','12');
+SELECT*FROM phone_shop;
